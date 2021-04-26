d26a76493423f7c0d32c8b31c487e4e61640bd56
llmir <mechasop@gmail.com>
2021-04-26 12:55:37 +0300

    This is a new commit for what I originally planned to be an amendmend

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const CONTACTS = [
+    {id: 1, name: 'Ilmir', value: '+7-888-888-88-88', marked: false}
+]
+
+
+app.use(express.static(path.resolve(__dirname, 'client')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+} )
+
+app.listen(3000, () => console.log('Server has been started on port 3000...'))
